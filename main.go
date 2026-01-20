c75c65b7c9c21e711966171f7a0ecda27c0b3364
Rajneesh <rajneesh.kumar@innoscripta.com>
2026-01-21 00:28:12 +0530

    chore:add kick4

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	// Define routes
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World from deployment kick 3")
+		fmt.Fprintf(w, "Hello World from deployment kick 4")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
